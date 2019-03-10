6c15320e0d1e24ff0c44f394a0db31491d3cae4f
Paul Soucy <Paul Soucy>
2019-03-10 15:00:59 -0400

    working

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -16,11 +16,11 @@ class ThoughtPlot {
         }
       }
     );
-    
 
-    network.on("selectNode", onNodeClicked);
 
-    nodes.add([
+    this.network.on("selectNode", this.onNodeClicked.bind(this));
+
+    this.nodes.add([
       { id: 1, label: 'Node 1' },
       { id: 2, label: 'Node 2' },
       { id: 3, label: 'Node 3' },
@@ -29,7 +29,7 @@ class ThoughtPlot {
     ]);
 
 
-    edges.add([
+    this.edges.add([
       { from: 1, to: 3 },
       { from: 1, to: 2 },
       { from: 2, to: 4 },
@@ -48,65 +48,9 @@ class ThoughtPlot {
   }
 };
 
-var thoughtPlot = (function () {
+var thoughtPlot;
 
-  var nodes = new vis.DataSet();
-  var edges = new vis.DataSet();
-
-  var network = new vis.Network(document.getElementById('notegraph'), {
-    nodes: nodes,
-    edges: edges
-  },
-    {
-      layout: {
-        randomSeed: 1
-      }
-    });
-
-  var onNodeClicked = function (obj) {
-    network.focus(obj.nodes[0], {
-      scale: 1.2,
-      animation: {
-        duration: 1000,
-        easingFunction: "easeInOutCubic"
-      }
-    });
-  }
-
-  network.on("selectNode", onNodeClicked);
-
-  nodes.add([
-    { id: 1, label: 'Node 1' },
-    { id: 2, label: 'Node 2' },
-    { id: 3, label: 'Node 3' },
-    { id: 4, label: 'Node 4' },
-    { id: 5, label: 'Node 5' }
-  ]);
-
-
-  edges.add([
-    { from: 1, to: 3 },
-    { from: 1, to: 2 },
-    { from: 2, to: 4 },
-    { from: 2, to: 5 }
-  ]);
-
-  return {
-    loadNote: function (id) {
-      if (id === undefined) {
-        id = '';
-      }
-      var url = "/api/v1/note/" + encodeURIComponent(id);
-      $.getJSON(url, function (data) {
-        $('#note').html(data.html);
-      });
-
-    }
-  };
-
-});
-
-window.on('load', function () {
-  thoughtPlot = thoughtPlot();
+$(document).ready(function() {
+  thoughtPlot = new ThoughtPlot();
   thoughtPlot.loadNote();
 });
